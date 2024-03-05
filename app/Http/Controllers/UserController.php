11293a442a68bcc956adec6b16f00c3f02811f45
Yayun Eldina <yayuneldina02@gmail.com>
2024-03-05 13:26:55 +0700

    Praktikum 2.5 – Attribute Changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -65,16 +65,59 @@ class UserController extends Controller
         // );
         // return view('user', ['data' => $user]);
 
-         $user = UserModel::firstOrNew(
+        //  $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager33',
+        //         'nama' => 'Manager Tiga Tiga',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ],
+        // );
+        // $user->save();
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::Create(
+        //     [
+        //         'username' => 'manager55',
+        //         'nama' => 'Manager55',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ],
+        // );
+        // $user->username = 'manager56';
+        
+        // $user->isDirty(); //true
+        // $user->isDirty('username'); //true
+        // $user->isDirty('nama'); //false
+        // $user->isDirty(['nama', 'ussername']); //true
+
+        // $user->isClean(); //False
+        // $user->isClean('username'); // false
+        // $user->isClean('nama'); //true
+        // $user->isClean(['nama', 'username']); //false
+
+        // $User->save();
+
+        // $user->isDirty(); //false
+        // $user->isClean(); //true
+        // dd($user->isDirty());
+
+        $user = UserModel::Create(
             [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
+                'username' => 'manager11',
+                'nama' => 'Manager11',
                 'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
+                'level_id' => 2,
+            ]);
+        $user->username = 'manager12';
+
         $user->save();
-        return view('user', ['data' => $user]);
+
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); //true
+        $user->wasChanged('nama'); //false
+        dd($user->wasChanged(['nama', 'username'])); //true
 
         // // // tambah data user dengan Eloquent Model
         // // $data = [
