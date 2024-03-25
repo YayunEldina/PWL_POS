3678caab0ed8586e98e8e58acfdf34e6393529b2
Yayun Eldina <yayuneldina02@gmail.com>
2024-03-25 23:34:51 +0700

    E. Tugas_JS6

--- resources/views/m_user/create.blade.php
+++ resources/views/m_user/create.blade.php
@@ -1,11 +1,20 @@
-@extends('m_user/template') @section('content')
+@extends('layouts.app')
+
+{{-- customize layout section --}}
+
+@section('subtitle', 'User')
+@section('content_header_title', 'Home')
+@section('content_header_subtitle', 'User')
+
+@section('content')
     <div class="row mt-5 mb-5">
         <div class="col-lg-12 margin-tb">
             <div class="float-left">
-                <h2>Membuat Daftar User</h2>
+                <h2>Create User</h2 List>
             </div>
             <div class="float-right">
-                <a class="btn btn-secondary" href="{{ route('m_user.index') }}"> Kembali</a>
+                <a class="btn btn-secondary" href="{{ route('m_user.index') }}">
+                    Kembali </a>
             </div>
         </div>
     </div>
@@ -23,27 +32,38 @@
         @csrf
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
+                <strong>Level ID:</strong>
+                <input type="number" name="level_id" class="form-control" placeholder="Masukkan Level ID"></input>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
                 <strong>Username:</strong>
                 <input type="text" name="username" class="form-control" placeholder="Masukkan username"></input>
             </div>
         </div>
-
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>nama:</strong>
-                <input type="text" name="nama" class="form-control" placeholder="Masukkan nama"></input>
+                <input type="text" name="nama" class="form-control" placeholder="Enter name"></input>
             </div>
         </div>
-
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Password:</strong>
                 <input type="password" name="password" class="form-control" placeholder="Masukkan password"></input>
             </div>
         </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+        <div class="card-footer">
             <button type="submit" class="btn btn-primary">Submit</button>
         </div>
         </div>
     </form>
 @endsection
+
+
+{{-- @extends('m_user/template')
+@section('content')
+
+@endsection --}}
+
--- resources/views/m_user/show.blade.php
+++ resources/views/m_user/show.blade.php
@@ -1,8 +1,16 @@
-@extends('m_user/template') @section('content')
+@extends('layouts.app')
+
+{{-- customize layout section --}}
+
+@section('subtitle', 'User')
+@section('content_header_title', 'Home')
+@section('content_header_subtitle', 'User')
+
+@section('content')
     <div class="row mt-5 mb-5">
         <div class="col-lg-12 margin-tb">
             <div class="float-left">
-                <h2> Show User</h2>
+                <h2>Show User</h2>
             </div>
             <div class="float-right">
                 <a class="btn btn-secondary" href="{{ route('m_user.index') }}"> Kembali</a>
@@ -22,28 +30,24 @@
                 {{ $useri->level_id }}
             </div>
         </div>
-
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Username:</strong>
                 {{ $useri->username }}
             </div>
         </div>
-
-
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Nama:</strong>
                 {{ $useri->nama }}
             </div>
         </div>
-
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Password:</strong>
                 {{ $useri->password }}
             </div>
         </div>
-
     </div>
 @endsection
+
